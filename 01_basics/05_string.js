764e03883d453c05fb73690ebaa836d45c2d6021
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-13 08:13:29 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,7 @@
+const name="anshu"
+const repoCount = 50
+// console.log(name+repoCount)
+console.log(`helllo world ${name} and my repo is ${repoCount}`)
+const gameName = new String('anshuman')
+console.log(gameName[0]);
+console.log(gameName.toUpperCase())
