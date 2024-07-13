da4fd31c8354fa8f8c1e86343e623577be33e13b
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-13 05:36:19 +0000

    prifix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,5 +12,22 @@ let valueInNumber = Number(score)
 // true => 1, false=>0
 let isLoggedIn = 1
 let isBooleanLoggedIn = Boolean(isLoggedIn)
-console.log(isBooleanLoggedIn)
+// console.log(isBooleanLoggedIn)
 //1=>true, 0=>false, ""=>false,"anshu"=>true
+
+
+//opration
+let value = 3
+let negValue = -value
+//console.log(negValue)
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
