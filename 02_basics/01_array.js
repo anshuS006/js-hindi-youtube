1a49c432c7e2b6f810ea94c46c26093f29f9cef3
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-13 14:00:33 +0000

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+// const myArray = [1,2,3,4,true,"hitesh"]
+// const myArray2 = new Array(0,2,3,4);
+// console.log(myArray[0])
+// slice splice
+const myArr = [1,2,3,4,5,6]
+console.log("A",myArr)
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+console.log("B",myArr)
+const myn2 = myArr.splice(1,3)
+console.log("C",myArr)
+console.log(myn2)
+
