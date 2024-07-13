a2447a52a1824d89bed788356b333ea5302c5b93
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-13 05:36:29 +0000

    prifix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,14 +20,3 @@ let isBooleanLoggedIn = Boolean(isLoggedIn)
 let value = 3
 let negValue = -value
 //console.log(negValue)
-let x = 3;
-const y = x++;
-
-console.log(`x:${x}, y:${y}`);
-// Expected output: "x:4, y:3"
-
-let a = 3;
-const b = ++a;
-
-console.log(`a:${a}, b:${b}`);
-// Expected output: "a:4, b:4"
