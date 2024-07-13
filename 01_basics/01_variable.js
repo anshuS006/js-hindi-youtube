8dbf5919c958efe3be19658fa2831e0fa8d14c22
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-13 04:41:43 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144526637
+let accountEmail="anshu@gmail.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+let accountState
+
+accountEmail = "ansu2@gmail.com"
+accountPassword  = "233444"
+accountCity = "delhi"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+//alert("hello") we are using node js, not browser
+let name = "anshuman"
+let age =20
+let isLoggedIn = false;
+
+//number => 2 to power of 53
+//bigint =>
+//string => "",''
+//boolean => true,false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+// object
+
+
+
+console.table([typeof name,typeof age,typeof isLoggedIn])
