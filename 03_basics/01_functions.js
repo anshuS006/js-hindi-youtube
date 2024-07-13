c92a9e840c4fe2d55e1aaee1c2efc9968512827d
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-13 17:20:22 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(num1,num2){
+    return num1+num2
+}
+const result =addTwoNumbers(3,5)
+//console.log("result ",result);
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+const loginMessage = loginUserMessage("anshuman")
+console.log(loginMessage);
