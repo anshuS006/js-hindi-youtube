d71756801eca1e26b3fbf96c8fd1ba3d7dc3fe1d
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-13 05:09:44 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = false
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abs" => NaN
+// true => 1, false=>0
+let isLoggedIn = 1
+let isBooleanLoggedIn = Boolean(isLoggedIn)
+console.log(isBooleanLoggedIn)
+//1=>true, 0=>false, ""=>false,"anshu"=>true
