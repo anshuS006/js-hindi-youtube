ce045226066602045c88254e52674d166e69ba20
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-13 16:39:44 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -12,5 +12,12 @@ const obj3={5:"a",6:"b"}
 const obj4 ={...obj1,...obj2,...obj3}
 //console.log(obj4)
 //console.log(tendarUser)
+const course = {
+    courseName:"Js course",
+    price:"999",
+    courseInstructor:"anshu"
+}
+const {courseInstructor:instructor}=course
+console.log(instructor);
 
 
