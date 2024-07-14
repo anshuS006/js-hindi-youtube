d75af9aaa75277fe63392173e17e9ea26a9240c1
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2024-07-14 11:01:38 +0000

    scope

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,4 +7,26 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 const loginMessage = loginUserMessage("anshuman")
-console.log(loginMessage);
+//console.log(loginMessage);
+function calculateCartPrice(...num1){
+return num1
+}
+console.log(calculateCartPrice(200,300,400))
+const user={
+    username:"anshum",
+    price:29
+}
+function handalObjects(anyObjects){
+    console.log(`user name is ${anyObjects.username} and price is ${anyObjects.price}`);
+}
+//handalObjects(user)
+handalObjects({
+    username:"uma",
+    price:45
+})
+const myNewArray = [100,200,300,400]
+function returnSecondValue(getArray){
+return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100,200,300]))
---
+++ 03_basics/02_scops.js
@@ -0,0 +1,25 @@
+let a=300
+if(true){
+    let a=30
+}
+
+//console.log(a);
+function one(){
+    const username = "anshuman"
+
+    function two(){
+        const website = "youtube"
+        //console.log(username)
+    }
+    two()
+}
+one()
+function addOne(num){
+return num+1
+}
+addOne(5)
+
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
